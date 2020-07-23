5fb524ec211bac69e6324d28940a10a502e3c75d
jSavedra <juansavedra827@gmail.com>
2020-07-23 17:58:00 -0300

    initial commit

--- src/com/savx/entity/Shoot.java
+++ src/com/savx/entity/Shoot.java
@@ -6,32 +6,30 @@ import java.awt.image.BufferedImage;
 
 import com.savx.main.Game;
 import com.savx.world.Camera;
-import com.savx.world.World;
 
 public class Shoot extends Entity {
 
 	private double dx;
 	private double dy;
 	private double spd = 3;
-	
+
 	private int timer = 0;
 	private int maxTimer = 120;
 
 	public Shoot(int x, int y, int width, int height, double dx, double dy, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
-		
+
 		this.dx = dx;
 		this.dy = dy;
-		
+
 	}
 
 	public void update() {
 		x += dx * spd;
 		y += dy * spd;
-		
+
 		timer++;
-		if (timer > maxTimer)
-		{
+		if (timer > maxTimer) {
 			Game.shoot.remove(this);
 			return;
 		}
